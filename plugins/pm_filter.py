5997b64b78ec97af73811fdb4ebe2ac2bffce71a
Jagadeeshgowdayt <98593308+Jagadeeshgowdayt@users.noreply.github.com>
2023-10-15 16:20:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2058,7 +2058,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             [
                 InlineKeyboardButton("Hero List", callback_data="herolist"
             ],[
-                InlineKeyboardButton(" categories / type", callback_data="type0"
+                InlineKeyboardButton(" categories / type", callback_data="type0")
             ],
             [
                 InlineKeyboardButton("Hᴏᴍᴇ", callback_data="start"),
